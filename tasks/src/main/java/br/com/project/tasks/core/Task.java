380fd5a93a594f3bbf36c7fb38f467ff50553d49
EliasBRodrigues <elias.rborges3@gmail.com>
2025-08-13 19:54:28 -0300

    feat: implements Builder class

--- tasks/src/main/java/br/com/project/tasks/core/Task.java
+++ tasks/src/main/java/br/com/project/tasks/core/Task.java
@@ -11,9 +11,72 @@ public class Task {
     private Integer priority;
     private TaskState state;
 
+    public Task(Builder builder){
+        this.title = builder.title;
+        this.description = builder.description;
+        this.priority = builder.priority;
+        this.state= builder.state;
+    }
+
+    public Task(){}
+
     // adicionando tarefa na lista em service
-    public Task newTask(){
+    public Task newTask() {
         TaskService.tasks.add(this);
         return this;
+    }
+
+    public Task insert(){
+        return builderFrom(this).withTaskState(TaskState.INSERT).build();
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+
+    public static class Builder {
+        private String title;
+        private String description;
+        private Integer priority;
+        private TaskState state;
+
+        public Builder(Task task) {
+            this.title = task.title;
+        this.description = task.description;
+        this.priority = task.priority;
+        this.state= task.state;
+        }
+
+        public Builder() {
+        }
+
+        public Builder withTitle(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder withDescription(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Builder withPriority(Integer priority){
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder withTaskState(TaskState state){
+            this.state = state;
+            return this;
+        }
+
+        public Task build(){
+            return new Task(this);
+        }
+    }
+
+    public static Builder builderFrom(Task task){
+        return new Builder(task);
     }
 }
