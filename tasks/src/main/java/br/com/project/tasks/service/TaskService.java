76aeb8cdb4d4999d86001a0d4a90d30b7581e98e
EliasBRodrigues <elias.rborges3@gmail.com>
2025-08-18 20:09:48 -0300

    update: change static methods and lists to save in database

--- tasks/src/main/java/br/com/project/tasks/service/TaskService.java
+++ tasks/src/main/java/br/com/project/tasks/service/TaskService.java
@@ -1,29 +1,35 @@
 package br.com.project.tasks.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import br.com.project.tasks.core.Task;
+import br.com.project.tasks.data.repository.TaskRepository;
 import reactor.core.publisher.Mono;
 
 @Service
 public class TaskService {
-    public static List<Task> tasks = new ArrayList<>();
+    // public static List<Task> tasks = new ArrayList<>();
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     // Mono: fluxo de dados que tem um item/ou nenhum
     public Mono<Task> insert(Task task) {
         return Mono.just(task)
-        .map(Task::insert) // objetos simples
-        .flatMap(it -> this.save(it)); // ou this::save -> funcoes/lambda
+                .map(Task::insert) // objetos simples
+                .flatMap(it -> this.save(it)); // ou this::save -> funcoes/lambda
     }
 
-    public Mono<List<Task>> list(){
-        return Mono.just(tasks);
+    public Mono<List<Task>> list() {
+        return Mono.just(taskRepository.findAll());
     }
 
-    private Mono<Task> save(Task task){
-        return Mono.just(task).map(Task :: newTask);
+    private Mono<Task> save(Task task) {
+        return Mono.just(task).map(taskRepository::save);
     }
 }
