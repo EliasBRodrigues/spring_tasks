129d97f901400434303ff5e34a3ba14555b840fe
EliasBRodrigues <elias.rborges3@gmail.com>
2025-08-18 19:39:05 -0300

    feat: TaskDTO class

---
+++ tasks/src/main/java/br/com/project/tasks/core/model/TaskDTO.java
@@ -0,0 +1,42 @@
+package br.com.project.tasks.core.model;
+
+import br.com.project.tasks.core.constant.TaskState;
+
+public class TaskDTO {
+    String title;
+    String description;
+    Integer priority;
+    TaskState state;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public TaskState getState() {
+        return this.state;
+    }
+
+    public void setState(TaskState state) {
+        this.state = state;
+    }
+}
