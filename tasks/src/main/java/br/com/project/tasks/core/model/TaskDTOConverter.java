2c52edab0c03817ad5961d8fa4b429aa39229d0c
EliasBRodrigues <elias.rborges3@gmail.com>
2025-08-23 21:28:34 -0300

    fix: Id variable added get-set

--- tasks/src/main/java/br/com/project/tasks/core/model/TaskDTOConverter.java
+++ tasks/src/main/java/br/com/project/tasks/core/model/TaskDTOConverter.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 import br.com.project.tasks.core.Task;
+import br.com.project.tasks.core.constant.TaskState;
 
 @Component
 public class TaskDTOConverter {
@@ -17,6 +18,7 @@ public class TaskDTOConverter {
         return Optional.ofNullable(task)
                 .map(source -> {
                     TaskDTO taskDTO = new TaskDTO();
+                    taskDTO.setId(source.getId());
                     taskDTO.setTitle(source.getTitle());
                     taskDTO.setDescription(source.getDescription());
                     taskDTO.setPriority(source.getPriority());
@@ -29,20 +31,25 @@ public class TaskDTOConverter {
     public Task convert(TaskDTO taskDTO) {
         return Optional.ofNullable(taskDTO)
                 .map(source -> Task.builder()
-                .withTitle(source.getTitle())
-                .withDescription(source.getDescription())
-                .withPriority(source.getPriority())
-                .withTaskState(source.getState())
-                .build())
+                        .withId(source.getId())
+                        .withTitle(source.getTitle())
+                        .withDescription(source.getDescription())
+                        .withPriority(source.getPriority())
+                        .withTaskState(source.getState())
+                        .build())
                 .orElse(null);
     }
 
     // transform to LIST DTO
-    public List<TaskDTO> convertList(List<Task> tasks){
+    public List<TaskDTO> convertList(List<Task> tasks) {
         return Optional.ofNullable(tasks).map(array -> array.stream().map(
-            this::convert
-        ).collect(Collectors.toList()))
-        
-        .orElse(new ArrayList<>());
+                this::convert).collect(Collectors.toList()))
+
+                .orElse(new ArrayList<>());
+    }
+
+    public Task convert(String id, String title, String description, int priority, TaskState state) {
+        return Task.builder().withId(id).withTitle(title).withDescription(description).withPriority(priority)
+                .withTaskState(state).build();
     }
 }
