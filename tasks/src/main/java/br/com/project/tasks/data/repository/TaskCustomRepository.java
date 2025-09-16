012413fa635af93149e1e804aee3e9bd2e886f6b
EliasBRodrigues <elias.rborges3@gmail.com>
2025-09-16 19:38:10 -0300

    update: new library to mongoDB -> MongoDBReactive

--- tasks/src/main/java/br/com/project/tasks/data/repository/TaskCustomRepository.java
+++ tasks/src/main/java/br/com/project/tasks/data/repository/TaskCustomRepository.java
@@ -7,22 +7,24 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import br.com.project.tasks.core.Task;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class TaskCustomRepository {
-    private final MongoOperations mongoOperations;
+    private final ReactiveMongoOperations mongoOperations;
 
-    public TaskCustomRepository(MongoOperations mongoOperations) {
+    public TaskCustomRepository(ReactiveMongoOperations mongoOperations) {
         this.mongoOperations = mongoOperations;
     }
 
-    public Page<Task> findPaginated(Task task, Integer page, Integer size) {
+    public Mono<Page<Task>> findPaginated(Task task, Integer page, Integer size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("title").ascending());
 
         ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("priority", "state");
@@ -38,8 +40,13 @@ public class TaskCustomRepository {
             query.addCriteria(Criteria.where("state").is(task.getState()));
         }
 
-        return PageableExecutionUtils.getPage(mongoOperations.find(query, Task.class), pageable, () ->
-                mongoOperations.count(query, Task.class));
+        // return PageableExecutionUtils.getPage(mongoOperations.find(query, Task.class), pageable, () ->
+        //         mongoOperations.count(query, Task.class));
+
+        return mongoOperations.find(query, Task.class)
+            .collectList()
+            .zipWith(mongoOperations.count(Query.query(Criteria.byExample(example)), Task.class))
+            .map(tuple -> PageableExecutionUtils.getPage(tuple.getT1(), pageable, tuple::getT2));
 
     }
 }
--- tasks/src/main/java/br/com/project/tasks/data/repository/TaskRepository.java
+++ tasks/src/main/java/br/com/project/tasks/data/repository/TaskRepository.java
@@ -1,11 +1,12 @@
 package br.com.project.tasks.data.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+// import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.project.tasks.core.Task;
 
 @Repository
-public interface TaskRepository extends MongoRepository<Task, String>{
+public interface TaskRepository extends ReactiveMongoRepository<Task, String>{
     
 }
