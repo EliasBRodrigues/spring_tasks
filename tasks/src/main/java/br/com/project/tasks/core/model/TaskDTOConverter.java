62885736e4158e1b7a4974543f9f8ea7172273db
EliasBRodrigues <elias.rborges3@gmail.com>
2025-09-15 21:41:59 -0300

    delete: libraries not use

--- tasks/src/main/java/br/com/project/tasks/core/model/TaskDTOConverter.java
+++ tasks/src/main/java/br/com/project/tasks/core/model/TaskDTOConverter.java
@@ -1,9 +1,6 @@
 package br.com.project.tasks.core.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
